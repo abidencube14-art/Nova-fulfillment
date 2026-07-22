3b3d087dbb118f2f3aab1704b6d10249573b0efb
abidencube14-art <abidencube14@gmail.com>
2026-07-22 16:16:42 +0200

    Rename and implement PesaPal authentication function

--- pesapal/auth.js
+++ pesapal/auth.js
@@ -1,14 +1,42 @@
 const axios = require("axios");
 const config = require("../config/settings");
 
-async function getPesapalToken() {
 
-    console.log("Connecting to PesaPal...");
+async function authenticatePesapal() {
 
-    return "TOKEN_PENDING";
+    try {
+
+        const response = await axios.post(
+            "https://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken",
+
+            {
+                consumer_key: config.pesapal.consumerKey,
+                consumer_secret: config.pesapal.consumerSecret
+            },
+
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+
+
+        return response.data.token;
+
+
+    } catch(error) {
+
+        console.log("PesaPal authentication failed");
+
+        console.log(error.message);
+
+        return null;
+    }
 
 }
 
+
 module.exports = {
-    getPesapalToken
+    authenticatePesapal
 };
