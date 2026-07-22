66cb778cbb47003f447520549374943d5a4c9935
abidencube14-art <abidencube14@gmail.com>
2026-07-22 16:06:07 +0200

    Implement getPesapalToken function in auth.js

---
+++ pesapal/auth.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const config = require("../config/settings");
+
+async function getPesapalToken() {
+
+    console.log("Connecting to PesaPal...");
+
+    return "TOKEN_PENDING";
+
+}
+
+module.exports = {
+    getPesapalToken
+};
